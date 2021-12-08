0c173e57820577586acd4c90baf12ee90cc16413
Дима Баранов <Dimabaranov17@rambler.ru>
2021-12-08 16:29:55 +0300

    add more logic

--- SkillCoacher/Pages/Submit.cshtml.cs
+++ SkillCoacher/Pages/Submit.cshtml.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using System.IO;
+using Model.Context;
+using Model.Models;
 namespace SkillCoacher.Pages
 {
     public class SubmitModel : PageModel
     {
+        string s;
         public void OnGet()
         {
+      
+        }
+        public void Add()
+        {
+            int a = 5;
+        }
+        public void OnPost(string name, string description, string content, string imageName = "aaa.jpg")
+        {
+            using(var db = new SkillCoacherContext())
+            {
+                db.Courses.Add(new Course { Name = name, Description = description, Content = content, TitleImagePath = imageName });
+                db.SaveChanges();
+            }
         }
     }
 }
